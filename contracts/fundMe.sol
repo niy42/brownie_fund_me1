00a73a3219232c623945a344bef3b0a6e4f19b2e
niy42 <obanlaniyi42@gmail.com>
2023-11-13 13:06:22 +0100

    modified

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -1,8 +1,11 @@
+// Project Title: fundMe.sol
+// Project completed by niy42
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// contract fundMe
 contract fundMe is ReentrancyGuard {
     AggregatorV3Interface internal priceFeed;
     address public owner;
@@ -18,6 +21,7 @@ contract fundMe is ReentrancyGuard {
     address[] public funders;
     mapping(address => uint256) public addresstoamountFunded;
 
+    // function to fund contract
     function fundme() public payable {
         uint256 minPay = 50 * 10 ** 18;
         require(
@@ -28,15 +32,18 @@ contract fundMe is ReentrancyGuard {
         funders.push(msg.sender);
     }
 
+    // returns price/data feed version
     function getVersion() public view returns (uint256) {
         return priceFeed.version();
     }
 
+    // returns eth/USD price
     function getPrice() public view returns (uint256) {
         (, int256 answer, , , ) = priceFeed.latestRoundData();
         return uint256(answer) * 1e10;
     }
 
+    // converts eth amount to its equivalent value in USD
     function getConversionRate(
         uint256 ethAmount
     ) public view returns (uint256) {
@@ -45,6 +52,7 @@ contract fundMe is ReentrancyGuard {
         return ethAmount_USD;
     }
 
+    // minimum entry fee for funding contract
     function getEntranceFee() public view returns (uint256) {
         uint256 minPay = 51 * 10 ** 18;
         uint256 precision = 1 * 1e18;
@@ -53,27 +61,34 @@ contract fundMe is ReentrancyGuard {
         return entryFee;
     }
 
+    // recieves plain ether without calldata i.e., when no function call matches
     receive() external payable {
         emit log(msg.sender, msg.value);
     }
 
+    // returns call data when no function call matches,...
+    // receives ether only when marked payable
     fallback() external {}
 
+    // modifies withdrawal function limited to onlyOwner
     modifier onlyOwner() {
         require(owner == msg.sender, "Caller is not Owner");
         _;
     }
 
+    // function to transfer contract ownership to newOwner
     function transferOWnership(address newOwner) public onlyOwner {
         emit OwnerSet(owner, newOwner);
         owner = newOwner;
     }
 
+    // function to renounce ownership to zero address
     function renounceOwnership() public onlyOwner {
         emit OwnerSet(owner, address(0));
         transferOWnership(address(0));
     }
 
+    // withdrawal function
     function withdraw() external onlyOwner {
         payable(owner).transfer(address(this).balance);
         uint8 i = 0;
